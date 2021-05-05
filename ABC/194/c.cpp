4a6f09a1750c375e1727d0834882a89332ce1f45
Akihiro Matsuyama <a@art.jp>
2021-05-05 13:37:22 +0900

    194

--- ABC/194/c.cpp
+++ ABC/194/c.cpp
@@ -8,25 +8,30 @@ using ll = long long;
 //以下debug用
 #define d(x) cout << #x << "; " << x << endl;
 #define p(x) cout << x << endl;
-#define f(x) for (int i = 0; i < x.size(); i++) cout << #x << "[" << i << "]; " << x[i] << endl;
+#define f(x) for (long unsigned int i = 0; i < x.size(); i++) cout << #x << "[" << i << "]; " << x[i] << endl;
 
-int a, b, c, d, e, f, g, i, j, m, n, ans = 0, counter = 0, num = 0;
-long long la, lb, lc, ld, lans = 0, lcounter = 0, lnum = 0;
-string str, str1, str2, str3;
 bool ok = false;
+vector<int> datas;
+const int inf = 1001001001;
+const ll INF = 1LL << 60; //無限大
 
 
 int main(){
-
-  cin >> a ;
-  vector<int> datas(a);
-  rep(i, a) cin >> datas[i];
-
-  rep(i,a){
-    rep1( j,i+1,a){
-      lans += pow((datas[i] - datas[j]), 2);
-    }
+  int N;
+  cin >> N;
+  vector<long long> A(N);
+  for (int i = 0; i < N; i++){
+    cin >> A[i];
   }
-p(lans)
-  return 0;
+
+  long long ans = 0;
+  for (int i = 0; i < N; i++){
+    ans += A[i] * A[i] * (N - 1);
+  }
+  long long sum = 0;
+  for (int i = 0; i < N; i++){
+    ans -= sum * A[i] * 2;
+    sum += A[i];
+  }
+	cout<<ans<<endl;
 }
