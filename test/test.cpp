6e633cd2a6f547e5ec6ba1e4bcc0457f5b17e1a0
Akihiro Matsuyama <a@art.jp>
2023-02-17 22:22:04 +0900

    no message

--- test/test.cpp
+++ test/test.cpp
@@ -1,57 +1,163 @@
+// https://qiita.com/akidon0000/items/019c1a6bcb494f0252ea
+#define _GLIBCXX_DEBUG
 #include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 using namespace std;
-using ll = long long int;
-using ld = long double;
 
-template<typename T> using v = vector<T>;
-template<typename T> using vv = vector<vector<T>>;
-template<typename T> using vvv = vector<vector<vector<T>>>;
 template<typename T> inline void errv(T& v) { for (auto x: v) cerr << x << " "; cerr << endl; }
-inline void errv(vector<bool>& v) { for (auto x: v) cerr << (x ? 1 : 0) << " "; cerr << endl; }
+template<typename T> inline void errvPair(T& v) { for (auto x: v) cerr << "first: " << x.first << " second: " << x.second << endl; cerr << endl; }
+template<typename T> inline void errvMap(T& v) { auto iter = v.begin(); while (iter != v.end()) { cout << "[" << iter->first << "," << iter->second << "]\n"; iter++;}}
 template<typename T> inline void dbgn(string n, T x) { cerr << n << ": " << x << endl; }
 template<typename T> inline void dbgn(string n, vector<T>& v) { cerr << n << ": "; errv(v); }
 template<typename T> inline void dbgn(string n, vector<vector<T>>& m) { cerr << n << ":" << endl; for (auto& v: m) errv(v); }
+template<> inline void dbgn(string n, vector<pair<string,long long int>>& p) { cerr << n << ":" << endl; errvPair(p); }
+template<> inline void dbgn(string n, vector<pair<long long int,string>>& p) { cerr << n << ":" << endl; errvPair(p); }
+template<> inline void dbgn(string n, vector<pair<long long int,long long int>>& p) { cerr << n << ":" << endl; errvPair(p); }
+template<> inline void dbgn(string n, map<string,long long int> p) { cerr << n << ": " << endl; errvMap(p); }
 
-#define _GLIBCXX_DEBUG
-#define rep(i, n) for (ll i = 0; i < (n); ++i)
-#define repp(i, c, n) for (ll i = c; i < (n); ++i)
-#define repa(i, a) for (auto i: a)
-#define all(a) (a).begin(),(a).end()
-#define rall(a) (a).rbegin(),(a).rend()
-#define so(v) sort((v).begin(), (v).end())
-#define rso(v) sort((v).rbegin(), (v).rend())
+#define _sel2(_1, _2, x, ...) x
+#define _sel3(_1, _2, _3, x, ...) x
+#define _repn(i, n) for (long long int i = 0; i < (n); i++)
+#define _reps(i, a, b) for (long long int i = (a); i < (b); i++)
+#define _rrepn(i, n) for (long long int i = (n) - 1; i >= 0; i--)
+#define _rreps(i, a, b) for (long long int i = (b) - 1; i >= (a); i--)
+#define _print(x) cout << x << endl;
+#define _printDec(x, num) cout << fixed << setprecision(num) << x << endl;
+#define _printBoolean(x, yes, no) cout << (x?yes:no) << endl;
+
+// 以下使用するコマンド
+using ll = long long int;
+using ld = long double;
+using pll = pair<ll, ll>;
+using psl = pair<string, ll>;
+using pls = pair<ll, string>;
+using vl = vector<ll>;
+using vb = vector<bool>;
+using vc = vector<char>;
+using vs = vector<string>;
+using vvl = vector<vector<ll>>;
+using vvb = vector<vector<bool>>;
+using vvc = vector<vector<char>>;
+using vvs = vector<vector<string>>;
+using vpll = vector<pair<ll, ll>>;
+using vpsl = vector<pair<string, ll>>;
+using vpls = vector<pair<ll, string>>;
+using mll = map<ll,ll>;
+using msl = map<string,ll>;
+using mls = map<ll,string>;
+
+#define all(a) (a).begin(),(a).end();
+#define rall(a) (a).rbegin(),(a).rend();
+#define so(v) sort((v).begin(), (v).end());
+#define rso(v) sort((v).rbegin(), (v).rend());
 #define len(x) ll((x).size())
+#define to_int(x) stoi(x)
 
-//p(出力) d(デバッグ *多次元配列も可能*)
-#define p(x) cout << x << endl;
+#define rep(i, ...) _sel2(__VA_ARGS__, _reps, _repn)(i, __VA_ARGS__)
+#define rrep(i, ...) _sel2(__VA_ARGS__, _rreps, _rrepn)(i, __VA_ARGS__)
+#define repa(it, lists) for (auto it: lists)
+#define p(...) _sel3(__VA_ARGS__, _printBoolean, _printDec, _print)(__VA_ARGS__)
 #define d(x) dbgn(#x, x);
 
 const ll INF = 1LL << 60;  //無限大
 const ll MOD = 1000000007; //10^9 + 7
 
-// 1000程度とする
-vector<int> lists(1000);
+#define SEMNAME "/sem"
+#define STRNAME "/string"
+#define MAX 1024
 
-void push(int y) {
+// sem開放
+void sem_free(sem_t *sem){
+  // semaphore解放
+  sem_destroy( sem );
 
-  lists.push_back(y);
+  // 共有メモリ開放
+  shm_unlink(SEMNAME);
 }
 
-int pop() {
-  int ans = lists[0];
-  lists.pop_back();
-  return ans;
+// str開放
+void str_free(char *str){
+  // 共有メモリ開放
+  shm_unlink(STRNAME);
 }
 
-int main(){
-  push(1);
-  push(2);
-  push(3);
-  p(pop())
-  p(pop())
-  p(pop())
-  return 0;
+int main()
+{
+  int p_id;
+  int status;
+  int return_code = 0;
+  int i;
+
+  int fd_sem;
+  void *ptr_sem;
+  int sem_num;
+  int fd_str;
+  void *ptr_str;
+  struct stat stat;
+
+  // semの共有メモリ作成
+  fd_sem = shm_open(SEMNAME, O_CREAT | O_RDWR, 0666);
+  ftruncate(fd_sem, MAX);
+
+  // semの共有メモリをポインタに割り当てる
+  ptr_sem = mmap(NULL, stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd_sem, 0);
+
+  // strの共有メモリ作成
+  fd_str = shm_open(STRNAME, O_CREAT | O_RDWR, 0666);
+  ftruncate(fd_str, MAX);
+
+  // strの共有メモリをポインタに割り当てる
+  ptr_str = mmap(NULL, stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd_str, 0);
+
+  // semのポインタのアドレスをsemに割り当てる
+  sem_t *sem = (sem_t*)ptr_sem;
+
+  // strのポインタのアドレスをstrに割り当てる
+  char *str = (char*)ptr_str;
+
+  // sempahoreの初期化:プロセス間を可能でsemの値0から
+  if (sem_init( sem, 1, 0 ) < 0) {
+    perror("init");
+    sem_free(sem);
+    exit(EXIT_FAILURE);
+  }
+
+  strcpy(str,"Initial");
+  cout << str << endl;
+
+  while(1){
+    sem_getvalue(sem, &sem_num);
+    cout << "wait...[" << sem_num << "]" << endl;
+    if(sem_wait(sem) < 0){ // waitする
+        perror("wait");
+        sem_free(sem);
+        exit(EXIT_FAILURE);
+    }
+    if(strcmp(str, "end\n") == 0){
+        break;
+    }
+    cout << "> " << str;
+    sleep(1);
+  }
+  cout << "end" << endl;
+
+  if (sem_post(sem) < 0) { // postする
+    perror("post");
+    sem_free(sem);
+    exit(EXIT_FAILURE);
+  }
+
+  sem_free(sem);
+  str_free(str);
+  return return_code;
 }
-
-
-
